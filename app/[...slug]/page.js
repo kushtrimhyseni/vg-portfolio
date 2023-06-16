4f0be86116b6650cfcd08c76a93ae9fe527f3284
kushtriimhyseni <kushtrim@starlabspro.com>
2023-06-16 20:46:12 +0200

    Integrated prisma client

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -1,7 +1,4 @@
-import { singleProject } from "@utils/dummy";
-
 export default function Page() {
-  console.log(router);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       Single Product Page

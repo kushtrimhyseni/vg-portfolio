e0b669b1ff4d414757ec21b47bc40f232ceb4b0e
kushtrim.hyseni <kushtrim@starlabspro.com>
2023-06-16 13:37:00 +0200

    Added route for single project page

---
+++ app/[...slug]/page.js
@@ -0,0 +1,10 @@
+import { singleProject } from "@utils/dummy";
+
+export default function Page() {
+  console.log(router);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      Single Product Page
+    </main>
+  );
+}
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,44 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
+import { projects } from "@utils/dummy";
 
 const ProjectCard = () => {
   return (
     <div className="grid grid-cols-2 gap-4 md:gap-8 lg:gap-12 max-w-screen-xl w-full pt-0 md:pt-24">
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
-      <Image
-        src="/assets/product.webp"
-        width={160}
-        height={240}
-        className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
-      />
+      {projects.map((project) => {
+        return (
+          <Link href={project.project_name}>
+          <Image
+            src={project.image}
+            width={160}
+            height={240}
+            className="w-[160px] h-[240px] md:w-full md:h-full object-cover"
+          />
+        </Link>
+        )
+      })}
     </div>
   );
 };
